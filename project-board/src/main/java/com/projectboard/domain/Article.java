416e16709cbc323cd5010f2a3924d2a6326cebc2
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-01-16 00:27:49 +0900

    #4-AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시 반복 사용 되기 때문에 도메인과 직접 연관이 없기 때문에 추출해서 사용 @MappedSuperclass 를 이용해서 상속
    그 외 @DateTimeFormat 요소 추가 및 jpa 옵선 개선

--- project-board/src/main/java/com/projectboard/domain/Article.java
+++ project-board/src/main/java/com/projectboard/domain/Article.java
@@ -4,13 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -24,9 +18,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,17 +29,11 @@ public class Article {
 
     @Setter @Column private String hashtag; //해시태그
 
+    @ToString.Exclude
     @OrderBy("id")
     @OneToMany(mappedBy = "Article", cascade = CascadeType.ALL )
     // 순환 참조 문제 발생 할 수 있어서 처리
-    @ToString.Exclude
     Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;//수정자
 
     protected Article() {
 
