063ea83297fd807b7dd3fc88cb485b84d46233f3
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-01-23 01:40:33 +0900

    #2 - securityConfig 로그인 경로 제외
    기본 로그인 경로를 넣었는데 그런 경우 시큐리티에서 제공하는 로그인 페이지를 보여주지 못해서 해당 코드 삭제 하니 정상 동작. 코드를 전체적으로 람다식으로 수정

--- project-board/src/main/java/com/projectboard/config/SecurityConfig.java
+++ project-board/src/main/java/com/projectboard/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +16,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth-> auth.anyRequest().permitAll())
-                .formLogin(login -> login // 기본 로그인 페이지 활성화
-                        .loginPage("/login")
-                        .permitAll()
-                );// 기본 로그인 경로 설정
+                // CSRF 보호 기능 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                // CORS 보호 기능 비활성화
+                .cors(AbstractHttpConfigurer::disable)
+                // 모든 요청에 대해 접근 허용 (인증 절차 없음)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                // 기본 로그인 경로 설정
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                );
         return http.build();
     }
 }
