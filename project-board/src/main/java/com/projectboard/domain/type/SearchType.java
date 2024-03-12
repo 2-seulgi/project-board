2f3b4c47528e50f82d0dfbac9140aa0a154afe86
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-03-13 01:38:33 +0900

    #33 - 검색 기능을 위해 searchType 옵션 전달 테스트
    게시판 페이지의 검색바에서 검색 종류를 드롭다운 메뉴로 제공 할때 해당 값을 뷰가 알게 하지 않고 서버로 부터 받아옴

--- project-board/src/main/java/com/projectboard/domain/type/SearchType.java
+++ project-board/src/main/java/com/projectboard/domain/type/SearchType.java
@@ -1,5 +1,17 @@
 package com.projectboard.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
-    TITLE,CONTENT,HASHTAG,ID,NICKNAME
+    TITLE("제목"),
+    CONTENT("본문"),
+    HASHTAG("해시태그"),
+    ID("유저 ID"),
+    NICKNAME("닉네임");
+
+    SearchType(String description) {
+        this.description = description;
+    }
+
+    @Getter private final String description;
 }
